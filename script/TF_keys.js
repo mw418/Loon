d49aeb4956829a4a5ea0d156a192d7a603e963d7
mw418 <931897493@qq.com>
2022-11-23 11:26:07 +0800

    [FIX]兼容app获取信息

--- script/TF_keys.js
+++ script/TF_keys.js
@@ -5,16 +5,16 @@
 $persistentStore.write(null, 'request_id')
 let url = $request.url
 let key = url.replace(/(.*accounts\/)(.*)(\/apps)/, '$2')
-let session_id = $request.headers['X-Session-Id']
-let session_digest = $request.headers['X-Session-Digest']
-let request_id = $request.headers['X-Request-Id']
-let ua = $request.headers['User-Agent']
+let session_id = $request.headers['X-Session-Id'] || $request.headers['x-session-id']
+let session_digest = $request.headers['X-Session-Digest'] || $request.headers['x-session-digest']
+let request_id = $request.headers['X-Request-Id'] || $request.headers['x-request-id']
+let ua = $request.headers['User-Agent'] || $request.headers['user-agent']
 $persistentStore.write(key, 'key')
 $persistentStore.write(session_id, 'session_id')
 $persistentStore.write(session_digest, 'session_digest')
 $persistentStore.write(request_id, 'request_id')
 $persistentStore.write(ua, 'tf_ua')
-console.log($persistentStore.read('request_id'))
+console.log($request.headers)
 if ($persistentStore.read('request_id') !== null) {
   $notification.post('请关闭本脚本', '信息获取成功','')
 
