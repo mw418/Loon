80718b02277ca9c091357faefc55958d386e4d2c
mw418 <931897493@qq.com>
2022-12-06 19:53:06 +0800

    [FIX]自动获取app_id

--- script/TF_keys.js
+++ script/TF_keys.js
@@ -1,24 +1,44 @@
 /*
-脚本作者：DecoAri
-引用地址：https://github.com/DecoAri/JavaScript/blob/main/Surge/TF_keys.js
+引用地址：https://raw.githubusercontent.com/chouchoui/QuanX/master/Scripts/testflight/TF_keys.js
 */
-$persistentStore.write(null, 'request_id')
-let url = $request.url
-let key = url.replace(/(.*accounts\/)(.*)(\/apps)/, '$2')
-let session_id = $request.headers['X-Session-Id'] || $request.headers['x-session-id']
-let session_digest = $request.headers['X-Session-Digest'] || $request.headers['x-session-digest']
-let request_id = $request.headers['X-Request-Id'] || $request.headers['x-request-id']
-let ua = $request.headers['User-Agent'] || $request.headers['user-agent']
-$persistentStore.write(key, 'key')
-$persistentStore.write(session_id, 'session_id')
-$persistentStore.write(session_digest, 'session_digest')
-$persistentStore.write(request_id, 'request_id')
-$persistentStore.write(ua, 'tf_ua')
-console.log($request.headers)
-if ($persistentStore.read('request_id') !== null) {
-  $notification.post('请关闭本脚本', '信息获取成功','')
 
-} else {
-  $notification.post('信息获取失败','请打开MitM开关','')
+const reg1 = /^https:\/\/testflight\.apple\.com\/v3\/accounts\/(.*)\/apps$/;
+const reg2 = /^https:\/\/testflight\.apple\.com\/join\/(.*)/;
+if (reg1.test($request.url)) {
+    $persistentStore.write(null, 'request_id')
+    let url = $request.url
+    let key = url.replace(/(.*accounts\/)(.*)(\/apps)/, '$2')
+    let session_id = $request.headers['X-Session-Id'] || $request.headers['x-session-id']
+    let session_digest = $request.headers['X-Session-Digest'] || $request.headers['x-session-digest']
+    let request_id = $request.headers['X-Request-Id'] || $request.headers['x-request-id']
+    let ua = $request.headers['User-Agent'] || $request.headers['user-agent']
+    $persistentStore.write(key, 'key')
+    $persistentStore.write(session_id, 'session_id')
+    $persistentStore.write(session_digest, 'session_digest')
+    $persistentStore.write(request_id, 'request_id')
+    $persistentStore.write(ua, 'tf_ua')
+    console.log($request.headers)
+    if ($persistentStore.read('request_id') !== null) {
+      $notification.post('请关闭本脚本', '信息获取成功','')
+
+    } else {
+      $notification.post('信息获取失败','请打开MitM开关','')
+    }
 }
+if (reg2.test($request.url)) {
+  let appId = $persistentStore.read("APP_ID");
+  if (!appId) {
+    appId = "";
+  }
+  let arr = appId.split(",");
+  const id = reg2.exec($request.url)[1];
+  arr.push(id);
+  arr = unique(arr).filter((a) => a);
+  if (arr.length > 0) {
+    appId = arr.join(",");
+  }
+  $prefs.setValueForKey(appId, "APP_ID");
+  $notify("TestFlight自动加入", `已添加APP_ID: ${id}`, `当前ID: ${appId}`);
+}
+
 $done({})
