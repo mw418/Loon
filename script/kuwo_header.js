81d24f78b1ce5ca0709f1f8199cf3273550fac76
mw418 <931897493@qq.com>
2023-04-02 10:58:47 +0800

    fix_error

--- script/kuwo_header.js
+++ script/kuwo_header.js
@@ -1,10 +1,10 @@
 let method = $request.method;
-let body = $response.body;
+let body = $request.body;
 console.log(method, body)
 if(method == 'post'){
     //替换请求头
     
-    $response.body = body.replace(/(?<=ids\=)\d+/, 6588259)
-    console.log('开始替换', $response.body)
+    $request.body = body.replace(/(?<=ids\=)\d+/, 6588259)
+    console.log('开始替换', $request.body)
 }
 $done({})
