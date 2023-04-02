f8939844eb9383e7e8d9201905188dcb8c5a1007
mw418 <931897493@qq.com>
2023-04-02 11:40:57 +0800

    missing $

--- script/kuwo_header.js
+++ script/kuwo_header.js
@@ -2,6 +2,6 @@ let method = $request.method;
 if(method.toLowerCase() == 'post'){
     //替换请求头
     body = $request.body.replace(/(?<=ids\=)\d+/, 6588259)
-    console.log(`替换后的请求体{body}`)
+    console.log(`替换后的请求体${body}`)
 }
 $done({body:body})
