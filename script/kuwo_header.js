2914a8696dcd3a08836c79205713829108d09dbe
mw418 <931897493@qq.com>
2023-04-02 11:37:03 +0800

    fix error

--- script/kuwo_header.js
+++ script/kuwo_header.js
@@ -1,10 +1,7 @@
 let method = $request.method;
-let body = $request.body;
-console.log(method, body)
-if(method == 'post'){
+if(method.toLowerCase() == 'post'){
     //替换请求头
-    
-    $request.body = body.replace(/(?<=ids\=)\d+/, 6588259)
-    console.log('开始替换', $request.body)
+    body = $request.body.replace(/(?<=ids\=)\d+/, 6588259)
+    console.log(`替换后的请求体{body}`)
 }
-$done({})
+$done({body:body})
