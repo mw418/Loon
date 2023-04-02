182262c564154bd83e2fa91d223edf0b71949530
mw418 <931897493@qq.com>
2023-04-02 11:43:43 +0800

    [add]readme

--- script/kuwo_header.js
+++ script/kuwo_header.js
@@ -1,3 +1,8 @@
+/* 
+author=lucky
+http-request ^.*?kuwo\.cn/music\.pay\?newver=3$ script-path=https://raw.githubusercontent.com/mw418/Loon/main/script/kuwo_header.js, tag=替换请求头, requires-body = true
+ */
+
 let method = $request.method;
 if(method.toLowerCase() == 'post'){
     //替换请求头
